3582ef0c4d94df41398b2f57dcebb4f7af440959
Helder Avila <helderavila2016@gmail.com>
2020-02-10 13:32:31 -0300

    release

---
+++ src/routes.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+
+import Main from './pages/Main';
+import Repository from './pages/Repository';
+
+function Routes() {
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route path="/" component={Main} exact />
+        <Route path="/repository/:repository" component={Repository} />
+      </Switch>
+    </BrowserRouter>
+  );
+}
+
+export default Routes;
